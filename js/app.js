bbd4c2cd01bbc8ddf8f4b4c9f7065875e762d4dd
roysonnovo <royson.novo@gmail.com>
2025-09-04 09:43:50 -0300

    Adicionar amigo

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+    
+    if (lista.textContent == ''){
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+    
+}
+
+function sortear() {
+
+}
+
+function remover() {
+
+}
