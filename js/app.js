f17d2d16eb37c5638f3c280dfeed7451553f9e86
roysonnovo <royson.novo@gmail.com>
2025-09-04 09:52:17 -0300

    Função sortear

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+let todosAmigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
+    todosAmigos.push(amigo.value);
     
     if (lista.textContent == ''){
         lista.textContent = amigo.value;
@@ -12,9 +15,21 @@ function adicionar() {
 }
 
 function sortear() {
-
+    embaralha(todosAmigos);
 }
 
-function remover() {
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
 
 }
